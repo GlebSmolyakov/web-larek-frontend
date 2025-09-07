08cd3fb19f695c8ae3c98b6e9725e84573037a0d
Gleb Smolyakov <gleb.smolyakov.05@inbox.ru>
2025-09-07 09:59:18 +0200

    сделал документацию

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import './scss/styles.scss';
+import { Api } from './components/base/api';
+import { API_URL, CDN_URL } from './utils/constants';
+
+
+const api = new Api(API_URL);
+
+async function main() {
+	try {
+		// Запрос на список продуктов
+		const products = await api.get('/product/');
+		console.log('Все товары:', products);
+	} catch (error) {
+		console.error('Ошибка запроса:', error);
+	}
+}
+
+main();
+
+
+
+
---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+//@todo интерфейсы данных сервера
+
+export interface IItem { // описание карточки, которая приходит от сервера
+	id: string;
+	title: string;
+	image: string;
+	category: string;
+	description: string;
+	price: number | null;
+}
+
+export interface IItemList { // описание списка карточек, которые приходят от сервера
+	items: IItem[];
+	total: number;
+}
+
+export type IItemCard = IItem; // описание одной, выбранной карточки
+
+export type ItemId = IItem['id']; // описание id карточки
+
+//@todo  интерфейс для заказа
+
+export type PaymentType = "online" | "offline"; // типизируем тип оплаты
+
+export interface IOrder { // интерфейс заказа для отправки на сервер
+	payment: PaymentType;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+export interface IOrderResponse { // интерфейс ответа от сервера созданного заказа, возвращает id и сумму заказа
+	id: string;
+	total: number;
+}
+
+export interface IOrderResponseError { // интерфейс ошибки сервера
+	error: string;
+}
+
+
